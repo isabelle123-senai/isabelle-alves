bd165070610cf06f656e42c1c15e3c71bc26b297
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-03-03 20:48:43 -0300

    Exercico06

--- exercicios/src/exercicios/exercicio06.java
+++ exercicios/src/exercicios/exercicio06.java
@@ -3,8 +3,66 @@ package exercicios;
 public class exercicio06 {
 
 	public static void main(String[] args) {
+
+		public static void main(String[] args) {
+
+			int vetor[] = { 8, 14, 2, 16, -5, 0 };
+
+			obterMaiorValor(vetor);
+			obterMenorValor(vetor);
+			obterMedia(vetor);
+		}
+
 		
+		public static void obterMaiorValor(int vetor[]) {
+
+			int maiorValor = Integer.MIN_VALUE;
+
+			
+
+			for (int i = 0; i < vetor.length; i++) {
+				if (vetor[i] > maiorValor) {
+					maiorValor = vetor[i];
+				}
+			}
+
+			System.out.println(maiorValor);
+
+		}
+
+		
+		public static void obterMenorValor(int vetor[]) {
+
+			int menorValor = Integer.MAX_VALUE;
+
+			
+
+			for (int i = 0; i < vetor.length; i++) {
+				if (vetor[i] < menorValor) {
+					menorValor = vetor[i];
+				}
+			}
+
+			System.out.println(menorValor);
+
+		}
+
+		
+		public static void obterMedia(int vetor[]) {
+
+			double soma = 0;
+
+			for (int i = 0; i < vetor.length; i++) {
+				soma += vetor[i];
+			}
+
+			double media = soma / vetor.length;
+
+			System.out.println(media);
+
+		}
+
+
 
 	}
 
-}
