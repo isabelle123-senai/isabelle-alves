266c1511ebbf4456e2d510c08633f33dd06131d4
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-03-17 20:48:57 -0300

    calcularProdutos

--- exercicios/src/produtos/calcularProduto.java
+++ exercicios/src/produtos/calcularProduto.java
@@ -10,7 +10,20 @@ public class calcularProduto {
 		p1.descricao ="Caderno em espiral tamanho médio";
 	    p1.precoUni = 4.50;
 	    p1.desconto = 15;
+	    
+	    produtos p2 = new produtos ();
 
+	    p2.nome = " caneta esf";
+	    p2.descricao ="Caneta esferográfica 5mm";
+	    p2.precoUni = 1.20;
+	    p2.desconto = 2;
+	    
+	    produtos p3 = new produtos ();
+	    
+	    p3.nome = "Esquadro";
+	    p3.descricao ="Esquadro de acrílico 20 cm";
+	    p3.precoUni =  2.35;
+	    p3.desconto = 10;
 	}
 
 }
