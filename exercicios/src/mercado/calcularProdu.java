d351a06b40e2f59a8cdd1504aa9ff15a3cf3e56b
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-03-17 19:02:45 -0300

    mercado

--- exercicios/src/mercado/calcularProdu.java
+++ exercicios/src/mercado/calcularProdu.java
@@ -9,26 +9,26 @@ public class calcularProdu {
 		m1.nome = "unidadeDeBlumenau";
 		m1.nmrMaca = 100;
 		m1.preçoMa =  4.00;
-		m1.nmrLara = 100;
+		m1.nmrLara = 200;
 		m1.preçoLara = 2.00;
 		
        mercado m2 = new mercado();
 		
-		m1.nome = "unidadeDeJoinville";
-		m1.nmrMaca = 100;
-		m1.preçoMa =  4.00;
-		m1.nmrLara = 100;
-		m1.preçoLara = 2.00;
+		m2.nome = "unidadeDeJoinville";
+		m2.nmrMaca = 200;
+		m2.preçoMa =  5.00;
+		m2.nmrLara = 300;
+		m2.preçoLara = 4.00;
 		
 		  mercado m3 = new mercado();
 			
-			m1.nome = "unidadeDeFlorianopolis";
-			m1.nmrMaca = 100;
-			m1.preçoMa =  4.00;
-			m1.nmrLara = 100;
-			m1.preçoLara = 2.00;
+			m3.nome = "unidadeDeFlorianopolis";
+			m3.nmrMaca = 400;
+			m3.preçoMa =  10.00;
+			m3.nmrLara = 800;
+			m3.preçoLara = 1.00;
 		
 
 	}
-
 }
+
