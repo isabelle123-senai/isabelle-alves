08d8d48e06681e76e9a762c484d9006b089e7747
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-04-23 19:38:29 -0300

    att 03 exe01

---
+++ exercicios/src/exercico03/exe01.java
@@ -0,0 +1,20 @@
+package exercico03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class exe01 {
+
+	 ArrayList<Integer> lista = new ArrayList<>();
+
+     lista.add(5);
+     lista.add(2);
+     lista.add(9);
+     lista.add(1);
+     lista.add(3);
+
+     Collections.sort(lista);
+
+     System.out.println("Lista ordenada: " + lista);
+}
+}
