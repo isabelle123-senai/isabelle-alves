da479ed5011f87d10340930cbe21738a4ae49477
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-04-28 19:06:22 -0300

    ex02

---
+++ exercicios/src/exercicio04/ex02.java
@@ -0,0 +1,13 @@
+package exercicio04;
+ 
+class Mercado {
+    String nomeMercado;
+    int numMacasVendidasAno;
+    double precoMaca;
+    int numLaranjasVendidasAno;
+    double precoLaranja;
+}
+
+public class ex02 {
+
+}
