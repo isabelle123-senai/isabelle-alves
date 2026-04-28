0b04b39915acd42ed6c5f9ea070ac5e736a3e2d5
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-04-28 19:05:07 -0300

    ex01

--- exercicios/src/exercicio04/ex01.java
+++ exercicios/src/exercicio04/ex01.java
@@ -16,4 +16,17 @@ class Pessoa {
 
 public class ex01 {
 
+    public static void main(String[] args) {
+
+       
+        Pessoa p1 = new Pessoa("Ana Silva", 'F', "10/05/2000", "Solteira");
+        Pessoa p2 = new Pessoa("Carlos Souza", 'M', "22/08/1995", "Casado");
+
+
+        System.out.println("Pessoa 1:");
+        System.out.println(p1.nome + " - " + p1.sexo + " - " + p1.dataNascimento + " - " + p1.estadoCivil);
+
+        System.out.println("\nPessoa 2:");
+        System.out.println(p2.nome + " - " + p2.sexo + " - " + p2.dataNascimento + " - " + p2.estadoCivil);
+    }
 }
