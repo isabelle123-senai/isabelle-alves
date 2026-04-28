62af8516a58a3696e9a95375dc8054b5ad11a31f
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-04-28 18:54:41 -0300

    ex03

---
+++ exercicios/src/exercico04/ex03.java
@@ -0,0 +1,24 @@
+package exercico04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Aluno {
+    String nome;
+    int idade;
+    String matricula;
+    String curso;
+    double mediaFinal;
+
+    public Aluno(String nome, int idade, String matricula, String curso, double mediaFinal) {
+        this.nome = nome;
+        this.idade = idade;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.mediaFinal = mediaFinal;
+    }
+}
+
+public class ex03 {
+
+}
