f5281d2118a333c178f09037afca95d4c081831f
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-04-28 19:04:49 -0300

    ex01

--- exercicios/src/exercicio04/ex01.java
+++ exercicios/src/exercicio04/ex01.java
@@ -1,5 +1,19 @@
 package exercicio04;
 
+class Pessoa {
+    String nome;
+    char sexo;
+    String dataNascimento;
+    String estadoCivil;
+
+    public Pessoa(String nome, char sexo, String dataNascimento, String estadoCivil) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.estadoCivil = estadoCivil;
+    }
+}
+
 public class ex01 {
 
 }
