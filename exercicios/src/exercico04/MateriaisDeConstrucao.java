e012b1345828904a8827757d7ffb9501008a9610
isabelle123-senai <isabellealvesgrana123@gmail.com>
2026-05-05 19:10:44 -0300

    materiasdecontrução

--- exercicios/src/exercico04/MateriaisDeConstrucao.java
+++ exercicios/src/exercico04/MateriaisDeConstrucao.java
@@ -5,5 +5,33 @@ import java.util.List;
 
 public class MateriaisDeConstrucao {
 
-	
+	private List<exe04> produtos = new ArrayList<>();
+
+    public void adicionarProduto(exe04 produto) {
+        produtos.add(produto);
+    }
+    public exe04 produtoMaisCaroPorMarca(String marca) {
+        exe04 maisCaro = null;
+
+        for (exe04 p : produtos) {
+            if (p.getMarca().equalsIgnoreCase(marca)) {
+                if (maisCaro == null || p.getPreco() > maisCaro.getPreco()) {
+                    maisCaro = p;
+                }
+            }
+        }
+        return maisCaro;
+    }
+
+    public List<exe04> produtosPorMarcaEPreco(String marca, double min, double max) {
+        List<exe04> resultado = new ArrayList<>();
+
+        for (exe04 p : produtos) {
+            if (p.getMarca().equalsIgnoreCase(marca) &&
+                p.getPreco() >= min && p.getPreco() <= max) {
+                resultado.add(p);
+            }
+        }
+        return resultado;
+    }
 }
